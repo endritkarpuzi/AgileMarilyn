2e8dcd4cf4d48b466e90eaae16c82da7e5b80bc3
Cezary Januszek <cezary.januszek@epfl.ch>
2020-10-30 11:55:42 +0100

    Folder rearrangement

--- Documentation/HelloWorld/Endrit_Karpuzi/HelloWorld Endrit Karpuzi.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorldEK{
-
-    public static void main(String[] args) {
-        System.out.println("Hello world from Endrit Karpuzi!");
-    }
-
-}
--- Documentation/HelloWorld/Firel_Issa/HelloWorld Firel Issa.java
+++
@@ -1,5 +0,0 @@
-public class Firel{
-    public static void main(String[] args) {
-        System.out.println("This will be printed");
-    }
-}
